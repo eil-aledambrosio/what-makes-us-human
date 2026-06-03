d1c916bd41768d6f086f56f669b5b7352b7c9b30
mattycarr <mattycarr03@gmail.com>
2026-06-03 17:24:01 +0200

    impostato sito

---
+++ js/logo.js
@@ -0,0 +1,32 @@
+paper.setup('logo-canvas');
+
+const LOGO_URL = 'logo.svg';
+const PADDING = 0.9;
+
+let logoItem = null;
+
+function fitLogo(item) {
+  const target = paper.view.bounds.scale(PADDING);
+  item.fitBounds(target, true);
+  item.position = paper.view.center;
+}
+
+function placeLogo(item) {
+  if (logoItem) {
+    logoItem.remove();
+  }
+  logoItem = item;
+  fitLogo(logoItem);
+}
+
+paper.project.importSVG(LOGO_URL, {
+  expand: true,
+  onLoad: placeLogo,
+  onError: () => console.error('Impossibile caricare', LOGO_URL),
+});
+
+paper.view.onResize = () => {
+  if (logoItem) {
+    fitLogo(logoItem);
+  }
+};
